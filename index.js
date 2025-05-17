086498b3aa76b1deaca5401dabc0956dbdb251a3
Huekusa <huekusam@outlook.jp>
2025-05-17 14:50:21 +0900

    Update 2025/05/17

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
 const terminal = document.getElementById("terminal");
-const lines = [
+const defaultLines = [
     "Connecting MyPage",
     "Loading[■■■■■■■■■■]100%",
-    "Complate Connect",
+    "Complete Connect",
     "Enter your order"
 ];
 
 let lineIndex = 0;
-let charIndex = 0;
-let currentLineText = "";
 let currentDir = "C:\\Users\\Guest>";
 
-function typeLine() {
+function typeLine(lines) {
     if (lineIndex >= lines.length) {
         inputPrompt();
         return;
     }
 
     const fullLine = lines[lineIndex];
-    if (charIndex === 0) {
-        const newLine = document.createElement("div");
-        newLine.className = "line";
-        terminal.appendChild(newLine);
+    const lineElement = document.createElement("div");
+    lineElement.className = "line";
+    terminal.appendChild(lineElement);
+
+    let i = 0;
+    function typeChar() {
+        lineElement.textContent += fullLine[i++];
+        
+        if (i < fullLine.length) {
+            setTimeout(typeChar, 30);
+        }else{
+            lineIndex++;
+            setTimeout(typeLine(lines), 300);
+        }
     }
-
-    const lineElement = terminal.lastChild;
-    currentLineText += fullLine[charIndex];
-    lineElement.textContent = currentLineText;
-
-    charIndex++;
-
-    if (charIndex < fullLine.length) {
-        setTimeout(typeLine, 30);
-    }else{
-        charIndex = 0;
-        currentLineText = "";
-        lineIndex++;
-        setTimeout(typeLine, 300);
-    }
-    
+    typeChar();
 }
 
 function inputPrompt() {
@@ -57,6 +50,51 @@ function inputPrompt() {
     terminal.appendChild(inputLine);
     input.focus();
 
+    input.addEventListener("keydown", function(e) {
+        if(e.key === "Enter") {
+            const userInput = input.value;
+            inputLine.remove();
+    
+            const executedLine = document.createElement("div");
+            executedLine.className = "line";
+            executedLine.textContent = currentDir + " " + userInput;
+            terminal.appendChild(executedLine);
+
+            cheakInputLine(userInput);
+
+            inputPrompt();
+        }
+    });
+
+}
+function cheakInputLine(userInput) {
+    const returnLine = document.createElement("div");
+    returnLine.className = "line";
+    const [base, ...args] = userInput.split(" ");
+
+    switch (base.toLowerCase()) {
+        case "now":
+            const now = new Date();
+            now.setTime(now.getTime() - now.getTimezoneOffset() * 60 * 1000);
+            const formattedNow = now.toISOString().replace("T", " ").substring(0,19);
+            returnLine.textContent = formattedNow;
+        break;
+            
+        case "help":
+            const helpText = [
+                "now 今日の日時を表示します",
+                "echo [TEXT] TEXTを表示します"
+            ];
+            // helpText.forEach(line => )
+            // returnLine.textContent = helpText;
+        break;
+
+        case "echo":
+            returnLine.textContent = args.join(" ");
+        break;
+        default:break;
+    }
+    terminal.appendChild(returnLine);
 }
 
-typeLine();
+typeLine(defaultLines);
